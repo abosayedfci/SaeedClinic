f11bff6bd873d002df076d2a1eff7f37c9025be2
Riham <Riham@DESKTOP-2S7NJVF>
2022-05-16 22:12:16 +0200

    Add project files.

---
+++ SaeedClinick/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SaeedClinick.MvcApplication" Language="C#" %>
---
+++ SaeedClinick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaeedClinick.Startup))]
+namespace SaeedClinick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
